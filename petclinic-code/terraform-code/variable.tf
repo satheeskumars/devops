1cef37d524886c4a980819f5ee9398eb4bbd4e1f
satheeskumars <satheeskumarbe@gmail.com>
2021-12-04 06:03:41 +0000

    terraform test

---
+++ petclinic-code/terraform-code/variable.tf
@@ -0,0 +1,23 @@
+variable "key_name" {
+  default = "terraform"
+}
+
+variable "pvt_key_name" {
+  default = "/root/.ssh/my-key.pem"
+}
+
+
+variable "sg_id" {
+  default = "sg-08ab1eedc41503f68"
+}
+
+
+variable "ami_id" {
+  default = "ami-0567e0d2b4b2169ae"
+}
+
+
+variable "region" {
+  default = "ap-south-1"
+}
+
